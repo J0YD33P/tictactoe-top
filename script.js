f99473e6732bebf5870b8c9a27c20acd24f308c1
J0YD33P <joyslearninghub@gmail.com>
2024-10-03 18:40:07 +0530

    Functionality of point A of the blueprint added

---
+++ script.js
@@ -0,0 +1,6 @@
+playerSelection = prompt("Welcome to TicTacToe! Choose between 'X' and 'O'").toLowerCase();
+opponentSelection = prompt("Is your opponent a human or a robot?").toLowerCase();
+
+function createBoard(){
+    
+}
