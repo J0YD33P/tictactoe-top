12e5e0775cc7e7457f24a77dbe9ecce1e6c3f25f
J0YD33P <joyslearninghub@gmail.com>
2024-10-04 01:40:40 +0530

    Functionality WIP

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-playerSelection = prompt("Welcome to TicTacToe! Choose between 'X' and 'O'").toLowerCase();
-opponentSelection = prompt("Is your opponent a human or a robot?").toLowerCase();
+letterSelection = prompt("Welcome to TicTacToe! Choose between 'X' and 'O'").toLowerCase();
+// opponentSelection = prompt("Is your opponent a human or a robot?").toLowerCase();
+gameLoop = true;
 
-function createBoard(){
-    
+const gameBoard = [['-','-','-'],['-','-','-'],['-','-','-']];
+
+function choosePosition(player){
+    if(player === 'x'){
+        letterPosition = prompt("Its 'X's turn. Choose your position: ");
+        gameBoard[Math.trunc(letterPosition/10)][letterPosition%10] = 'X';
+    }
+    else{
+        letterPosition = prompt("Its 'O's turn. Choose your position: ");
+        gameBoard[Math.trunc(letterPosition/10)][letterPosition%10] = 'O';
+    }
+
+    for (let row of gameBoard) {
+        const rowString = row.join('|');
+        console.log(rowString);
+    }
 }
+
+while(gameLoop){
+    if(letterSelection !== 'x' && letterSelection !== 'o'){
+        console.log("Invalid Selection.");
+    }
+    else if(letterSelection === 'x'){
+        choosePosition('x');
+    }
+    else{
+        choosePosition('o');
+    }
+}
+
+
+
