0a1cb43ce9009195b1fbd9b3eb08708d8ad663c7
J0YD33P <joyslearninghub@gmail.com>
2024-10-13 02:04:28 +0530

    Replaced prompt with button to allow live server to load

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-letterSelection = prompt("Welcome to TicTacToe! Choose between 'X' and 'O'").toLowerCase();
+const playerChoiceInput = document.querySelector('#playerChoice');
+const playGameButton = document.querySelector('button');
+
+playGameButton.addEventListener("click", () => {
+    const playerChoice = playerChoiceInput.value.toLowerCase();
+    if(playerChoice !== 'x' && playerChoice !== 'o'){
+        console.log("Invalid Selection.");
+    }
+    else{
+        choosePosition();
+    }
+});
+
 // opponentSelection = prompt("Is your opponent a human or a robot?").toLowerCase();
+// robotChoice = ['00', '01', '02', '10', '11', '12', '20', '21', '22'];
 gameLoop = true;
 
 const gameBoard = [['-','-','-'],['-','-','-'],['-','-','-']];
 
-function choosePosition(player){
-    if(player === 'x'){
-        letterPosition = prompt("Its 'X's turn. Choose your position: ");
-        gameBoard[Math.trunc(letterPosition/10)][letterPosition%10] = 'X';
+function choosePosition(){
+
+    letterPosition = prompt("Its 'X's turn. Choose your position: ");
+    gameBoard[Math.trunc(letterPosition/10)][letterPosition%10] = 'X';
+    
+    for (let row of gameBoard) {
+        const rowString = row.join('|');
+        console.log(rowString);
     }
-    else{
-        letterPosition = prompt("Its 'O's turn. Choose your position: ");
-        gameBoard[Math.trunc(letterPosition/10)][letterPosition%10] = 'O';
-    }
+
+    letterPosition = prompt("Its 'O's turn. Choose your position: ");
+    gameBoard[Math.trunc(letterPosition/10)][letterPosition%10] = 'O';
+    console.log(gameBoard);
 
     for (let row of gameBoard) {
         const rowString = row.join('|');
@@ -20,17 +37,8 @@ function choosePosition(player){
     }
 }
 
-while(gameLoop){
-    if(letterSelection !== 'x' && letterSelection !== 'o'){
-        console.log("Invalid Selection.");
-    }
-    else if(letterSelection === 'x'){
-        choosePosition('x');
-    }
-    else{
-        choosePosition('o');
-    }
-}
+
+
 
 
 
